173c61611e0f3de20df27f1bf8dc56671ab525f8
RRasiyani <RRasiyani@slb.com>
2021-12-23 10:46:17 -0600

    counter with async but does not work

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { counterState } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.scss']
 })
+// export class CounterOutputComponent implements OnInit, OnDestroy {
 export class CounterOutputComponent implements OnInit {
 
-  counter: number | undefined;
+  // counter: number | undefined;
 
-  constructor(private store: Store<{counter: {counter: number}}>) { }
+  counter$!: Observable<{ counter: number }>;
+
+  // counterSubscription: Subscription | undefined;
+
+  constructor(private store: Store<{counter: counterState}>) { }
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe( (data) => {
-      this.counter = data.counter;
-    })
-  }
+    this.counter$ = this.store.select('counter');
+    }
+
+
+  // ngOnInit(): void {
+  //   this.counterSubscription = this.store.select('counter').subscribe( (data) => {
+  //     this.counter = data.counter;
+  //   })
+  // }
+
+  // ngOnDestroy(){
+  //   this.counterSubscription?.unsubscribe();
+  // }
 
 }
