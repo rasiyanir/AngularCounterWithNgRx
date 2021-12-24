35c876f907bf2ba9e7950a1571b00591db8dd9e8
RRasiyani <RRasiyani@slb.com>
2021-12-24 11:14:59 -0600

    add post to the list

--- src/app/posts/add-posts/add-posts.component.ts
+++ src/app/posts/add-posts/add-posts.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Post } from 'src/app/models/posts.model';
+import { AppState } from 'src/app/store/app.state';
+import { addPost } from '../state/post.actions';
 
 @Component({
   selector: 'app-add-posts',
@@ -10,7 +14,7 @@ export class AddPostsComponent implements OnInit {
 
   postForm!: FormGroup;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.postForm = new FormGroup({
@@ -37,7 +41,12 @@ export class AddPostsComponent implements OnInit {
     if(this.postForm.invalid){
       return;
     }
-    console.log(this.postForm.value);
+    const post: Post = {
+      title: this.postForm.value.title,
+      description: this.postForm.value.description,
+    }
+
+    this.store.dispatch(addPost({post}));
   }
 
   showDescriptionErrors(){
--- src/app/posts/state/post.reducer.ts
+++ src/app/posts/state/post.reducer.ts
@@ -1,8 +1,17 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { addPost } from "./post.actions";
 import { initialState } from "./post.state";
 
 const _postReducer = createReducer(
-  initialState)
+  initialState,
+  on(addPost, (state, action) => {
+    let post = { ...action.post};
+    post.id = (state.posts.length + 1).toString();
+    return {
+      ...state,
+      posts: [...state.posts, post]
+    }
+  }))
 
 export function PostReducer(state: any, action: any) {
   return _postReducer(state, action);
