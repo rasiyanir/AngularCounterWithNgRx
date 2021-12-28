639af9d6fde4c6edf25fed9796386de4813d6253
RRasiyani <RRasiyani@slb.com>
2021-12-28 12:54:47 -0600

    After login send the token data to login success action as props

--- src/app/auth/state/auth.effects.ts
+++ src/app/auth/state/auth.effects.ts
@@ -15,7 +15,8 @@ export class AuthEffects {
       exhaustMap((action) => {
         return this.authService.login(action.email, action.password).pipe(
           map((data) => {
-            return loginSuccess();
+            const user = this.authService.formatUser(data);
+            return loginSuccess({ user });
           })
         );
       })
--- src/app/auth/state/auth.reducer.ts
+++ src/app/auth/state/auth.reducer.ts
@@ -1,7 +1,16 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { loginSuccess } from "./auth.actions";
 import { initialState } from "./auth.state";
 
-const _authReducer = createReducer(initialState)
+const _authReducer = createReducer(
+  initialState,
+  on(loginSuccess, (state, action) => {
+    console.log(action.user);
+    return {
+      ...state,
+      user: action.user,
+    }
+  }))
 
 export function authReducer(state: any, action: any) {
   return _authReducer(state, action);
