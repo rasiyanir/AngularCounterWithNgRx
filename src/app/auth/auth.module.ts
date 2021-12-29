2e271f4a90b7dd3823323e6b2c7673732167c625
RRasiyani <RRasiyani@slb.com>
2021-12-28 20:49:01 -0600

    Adding Signup component and action and also dispatch start spinner

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,19 +5,21 @@ import { RouterModule, Routes } from "@angular/router";
 import { EffectsModule } from "@ngrx/effects";
 import { LoginComponent } from "./login/login.component";
 import { AuthEffects } from "./state/auth.effects";
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: '', redirectTo: 'login'},
-      { path: 'login', component: LoginComponent}
+      { path: 'login', component: LoginComponent},
+      { path: 'signup', component: SignupComponent}
     ]
   }
 ]
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, SignupComponent],
   imports: [CommonModule,
     ReactiveFormsModule,
     EffectsModule.forFeature([AuthEffects]),
